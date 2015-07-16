4b6d2627169f331714ca01fa5942660c0b3eb71a
Zieghailo <isakov.m@gmail.com>
2015-07-16 17:14:43 +0200

    Support for copying directories

--- pycopy.py
+++ pycopy.py
@@ -6,7 +6,6 @@ import os
 def main():
     parser = argparse.ArgumentParser(description = "Copy file path to clipboard")
     parser.add_argument("files", 
-            type=argparse.FileType('r'),
             nargs='+')
     
     args = parser.parse_args()
@@ -16,7 +15,7 @@ def main():
             os.path.expanduser('~/.pycopy/clipboard.txt'), 
             'w')
     for file in args.files:
-        home_dir.write(cur_dir + '/' + file.name)
+        home_dir.write(cur_dir + '/' + file)
         home_dir.write("\n")
 
 if __name__ == "__main__":
